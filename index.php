72db9816267246a4d600b698f5c134edb4ca8501
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-10 17:35:07 +0530

    Delete product with id = PR003.

--- index.php
+++ index.php
@@ -88,43 +88,34 @@ $products = array(
     )
         );
         echo("<table>");
-       // echo("<tr><th>Category</th><th>Sub-Category</th><th>ID</th><th>Name</th><th>Brand</th></tr>");
+        echo("<tr><th>Category</th><th>Sub-Category</th><th>ID</th><th>Name</th><th>Brand</th></tr>");
 
  foreach($products as $cat1 =>$val1){
      
-    
+              
     foreach($val1 as $sub_cat=>$val2){
         
-       
+
         foreach($val2 as $sub_cat2 =>$val3)
         {   
-           // echo("<tr>");
-           // echo("<td> $cat1</td>"); 
-          //  
-           // 
-             if( $val3['brand']=="Samsung") {
-                echo("<br><br>");
-               
+            echo("<tr>");
+            echo("<td> $cat1</td>");
+            echo("<td> $sub_cat</td>");
+            //echo("<td> $sub_cat2 </td>");
+             if($val3['id']=="PR003"){
+                 unset($products[$cat1][$sub_cat][$sub_cat2]);
+             }
+            foreach($val3 as $sub_cat3=> $final_vall ){
                 
-                foreach($val3 as $sub_cat3=> $final_vall ){
+              
                 
-                    
-                 echo("$sub_cat3 ". " $final_vall <br>");
-                 
-                 
-             
-             
+                
+                echo("<td> $final_vall </td>");
                 
                   
                 
-                }
-                echo("Category: $cat1 <br>");
-
-                 echo("Sub-Category: $sub_cat <br>");
-               }
-           
-            
-             //echo("</tr>");
+             }
+             echo("</tr>");
              
            
 
@@ -139,6 +130,54 @@ $products = array(
 
  }
  echo("</table>");
+
+ echo("<table>");
+ echo("<tr><th>Category</th><th>Sub-Category</th><th>ID</th><th>Name</th><th>Brand</th></tr>");
+
+
+ echo("<br><br><br>");
+
+
+
+
+foreach($products as $cat1 =>$val1){
+
+       
+foreach($val1 as $sub_cat=>$val2){
+ 
+
+ foreach($val2 as $sub_cat2 =>$val3)
+ {   
+     echo("<tr>");
+     echo("<td> $cat1</td>");
+     echo("<td> $sub_cat</td>");
+     //echo("<td> $sub_cat2 </td>");
+     
+     foreach($val3 as $sub_cat3=> $final_vall ){
+         
+       
+         
+         
+         echo("<td> $final_vall </td>");
+         
+           
+         
+      }
+      echo("</tr>");
+      
+    
+
+ }
+
+
+ 
+}
+
+
+
+
+}
+echo("</table>");
 
 
 
