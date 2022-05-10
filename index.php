fdca8890ef91677419875ac75404e2eec2e1e4b1
vaibhavkumartiwari-cedcommerce <vaibhavkumartiwari@cedcoss.com>
2022-05-10 18:05:34 +0530

    5. Update product name = BIG-555 with id = PR002.

--- index.php
+++ index.php
@@ -102,8 +102,9 @@ $products = array(
             echo("<td> $cat1</td>");
             echo("<td> $sub_cat</td>");
             //echo("<td> $sub_cat2 </td>");
-             if($val3['id']=="PR003"){
-                 unset($products[$cat1][$sub_cat][$sub_cat2]);
+             if($val3['id']=="PR002"){
+                 $val3['name']="BIG_555";
+                 
              }
             foreach($val3 as $sub_cat3=> $final_vall ){
                 
@@ -129,56 +130,5 @@ $products = array(
 
 
  }
- echo("</table>");
-
- echo("<table>");
- echo("<tr><th>Category</th><th>Sub-Category</th><th>ID</th><th>Name</th><th>Brand</th></tr>");
-
-
- echo("<br><br><br>");
-
-
-
-
-foreach($products as $cat1 =>$val1){
-
-       
-foreach($val1 as $sub_cat=>$val2){
- 
-
- foreach($val2 as $sub_cat2 =>$val3)
- {   
-     echo("<tr>");
-     echo("<td> $cat1</td>");
-     echo("<td> $sub_cat</td>");
-     //echo("<td> $sub_cat2 </td>");
-     
-     foreach($val3 as $sub_cat3=> $final_vall ){
-         
-       
-         
-         
-         echo("<td> $final_vall </td>");
-         
-           
-         
-      }
-      echo("</tr>");
-      
-    
-
- }
-
-
- 
-}
-
-
-
-
-}
-echo("</table>");
-
-
 
 ?>
